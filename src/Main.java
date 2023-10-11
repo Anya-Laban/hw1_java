2dab31af89a79ca25a98abbe1f18fbf82395e707
Anya-Laban <laban.anya.a@gmail.com>
2023-10-11 14:02:00 +0300

    laba 1

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,26 @@ public class Main {
 
         final long C = 2;
 
-        final long A = 1;
-        final long N = 3;
-        final long B = 7;
-        final long M = 8;
+        final long A = 2; //початкове значення
+        final long B = 7; //кінцеве значення
 
-        long S = 0;
+        final long N = 1; //початкове значення
+        final long M = 8; //кінцеве значення
 
-        for (long i = A; i <= B ; i++){
-            for (long j = N; j <= M ; j++){
-                if(i != C) S += (i%j)/(i-C);
+
+        if (A > B || N > M) {
+            System.out.println("Некоректно введені початкові чи кінцеві значення");
+        }else{
+            long S = 0;
+
+            for (long i = A; i <= B ; i++){
+                for (long j = N; j <= M ; j++){
+                    if(i != C) S += (i%j)/(i-C);
+                }
             }
+            System.out.print("S = " + S);
         }
-        System.out.print("S = " + S);
+
+
     }
 }
